cb6c28ee3d0d4d11a8c7a3ef043aa630528b6de8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-03 15:11:41 +0530

    Take a snapshot if the provided path is not a `JSON` file

--- src/commands/graph.ts
+++ src/commands/graph.ts
@@ -1,8 +1,12 @@
 // Library
 import { readFileSync, writeFileSync } from "node:fs";
+import { extname } from "node:path";
+import { snapshot } from "../library/snapshot.js";
 import { generateForceDirectedTreeGraph } from "../library/forceDirectedGraph.js";
 
 // Type Definitions
+import type { Node } from "../class/Node.js";
+
 interface graphOptions {
     path: string,
     output: string,
@@ -11,14 +15,14 @@ type Graph = (options: graphOptions) => void;
 
 const command: Command<Graph> = {
     name: "graph",
-    description: "Visualize your workspace tree as a graph",
+    description: "Visualize the workspace tree as a graph",
     args: [
     ],
     options: [
         {
             name: "-p, --path [path]",
-            description: "The path to the JSON file to graph",
-            default: "./workspace.json"
+            description: "The path to the workspace to graph",
+            default: process.cwd()
         },
         {
             name: "-o, --output [output]",
@@ -31,8 +35,13 @@ const command: Command<Graph> = {
     run: async ({ path, output }) => {
         console.log('Creating graph...')
 
-        // Read the JSON file
-        const root = JSON.parse(readFileSync(path, 'utf-8'));
+
+        let root: Node
+        if (extname(path) === '.json') {
+            root = JSON.parse(readFileSync(path, 'utf-8'));
+        } else {
+            root = snapshot({ path, ignore: [], include: [], exclude: [] });
+        }
 
         // Generate the graph
         const svg = await generateForceDirectedTreeGraph(root, {
