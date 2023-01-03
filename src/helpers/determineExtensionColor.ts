44005012d71d3edfe2803acc2ec3f12e89c5d021
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-03 19:31:13 +0530

    Return a `fallbackColor` if the extension is not found

--- src/helpers/determineExtensionColor.ts
+++ src/helpers/determineExtensionColor.ts
@@ -5,16 +5,21 @@ import extensionColors from "./colorAssociation.json"; // This is a JSON file wi
 /**
  * Determines the color of the extension icon based on the file extension
  * @param path The path to the file
+ * @param fallbackColor The fallback color
  * @returns The color of the extension icon
  * @example
  * const color = determineExtensionColor("C:\\Users\\User\\Desktop\\file.js")
  * console.log(color) // #f7df1e
  */
-export function determineExtensionColor(path: string): string {
+export function determineExtensionColor(path: string, fallbackColor: string = "#000000"): string {
+    // Get the extension of the file
     const extension = extname(path).toLowerCase();
+    // Check if the extension is in the JSON file
     if (extension in extensionColors) {
+        // Return the color of the extension
         return extensionColors[extension as keyof typeof extensionColors]
     } else {
-        return "#000000"
+        // Return the fallback color
+        return fallbackColor;
     }
 }
