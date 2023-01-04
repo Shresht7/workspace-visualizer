5869e65ff7ecc42a4087c2772b590970f6e13ae1
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-04 19:23:39 +0530

    Test Node tree json output

--- apps/extension/src/extension.ts
+++ apps/extension/src/extension.ts
@@ -1,14 +1,32 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Node } from './class/Node';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-workspace-visualizer" is now active!');
+
+	// Get the current workspace
+	const workspaceUri = vscode.workspace.workspaceFolders?.[0].uri;
+
+	// Check if the workspace is defined
+	if (!workspaceUri) {
+		// Show an error message
+		vscode.window.showErrorMessage('No workspace is opened');
+		// Return
+		return;
+	}
+
+	// Create a new node
+	const node = new Node(workspaceUri);
+
+	// Build the tree
+	await node.buildTree();
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -22,7 +40,7 @@ export function activate(context: vscode.ExtensionContext) {
 		const panel = vscode.window.createWebviewPanel('fstree', 'FSTree', vscode.ViewColumn.One);
 
 		//	Set HTML Content
-		panel.webview.html = "<body>Hello World</body>";
+		panel.webview.html = JSON.stringify(node, null, 4);
 
 		//	When the panel is disposed
 		panel.onDidDispose(() => {
