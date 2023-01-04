2961752c2a596d3b8ad0ed8aef0faafc86d92458
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-04 20:12:08 +0530

    Setup `WorkspaceVisualizerPanel` properly
    Disable inline styles and scripts for security

--- apps/extension/src/views/WorkspaceVisualizerPanel.ts
+++ apps/extension/src/views/WorkspaceVisualizerPanel.ts
@@ -2,6 +2,9 @@
 import * as vscode from 'vscode';
 import { WebviewPanel } from "../class/WebviewPanel";
 
+//  Helpers
+import { getNonce } from '../helpers';
+
 // ==========================
 // WORKSPACE VISUALIZER PANEL
 // ==========================
@@ -21,7 +24,7 @@ export class WorkspaceVisualizerPanel extends WebviewPanel {
     public static get webviewOptions(): vscode.WebviewOptions {
         return {
             enableScripts: true,
-            localResourceRoots: [WorkspaceVisualizerPanel.extensionUri]
+            localResourceRoots: [vscode.Uri.joinPath(WorkspaceVisualizerPanel.extensionUri, "src", "view")]
         };
     };
 
@@ -56,12 +59,12 @@ export class WorkspaceVisualizerPanel extends WebviewPanel {
         );
 
         //	Set the webview's initial html content
-        this.update("");
+        this.update();
 
         //	Update the content based on view changes
         this._panel.onDidChangeViewState(e => {
             if (!this._panel.visible) { return; }
-            this.update("");
+            this.update();
         }, null, this._disposables);
 
         //	Listen for when the panel is disposed
@@ -70,84 +73,39 @@ export class WorkspaceVisualizerPanel extends WebviewPanel {
     }
 
     /** Update the HTML Content */
-    private async update(content: string) {
+    private async update() {
+        const nonce = getNonce();
+
+        const styleUri = this._panel.webview.asWebviewUri(
+            vscode.Uri.joinPath(WorkspaceVisualizerPanel.extensionUri, "src", "view", "style.css")
+        );
+
+        const scriptUri = this._panel.webview.asWebviewUri(
+            vscode.Uri.joinPath(WorkspaceVisualizerPanel.extensionUri, "src", "view", "script.js")
+        );
+
+        const content = `
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none';style-src ${this._panel.webview.cspSource}; img-src ${this._panel.webview.cspSource} https:; script-src 'nonce-${nonce}';" />
+				<link rel="stylesheet" href="${styleUri}" />
+				<script type="module" src="${scriptUri}" nonce="${nonce}"></script>
+				<title>Document</title>
+			</head>
+			<body>
+			</body>
+			</html>
+		`;
         this.setHTML(content);
-
     }
 
-    /** Set the Webview HTML content to show the Force-Directed Graph */
+    /** Show the Force-Directed Graph */
     public async showForceDirectedGraph(tree: Object) {
-        const content = this.getForceDirectedGraphHTMLContent(JSON.stringify(tree));
-        this.update(content);
-    }
-
-    /** Get the HTML content to show the Force-Directed Graph */
-    public getForceDirectedGraphHTMLContent(tree: string) {
-        return `
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Document</title>
-		</head>
-		<style>
-			html,body {
-				height: 100vh;
-				width: 100vw;
-			}
-		</style>
-		<body>		
-		</body>
-		<script type="module">
-			import * as d3 from "https://cdn.skypack.dev/d3@7"
-			
-			const width = 400
-			const height = 400
-			const root = d3.hierarchy(${tree})
-			const links = root.links()
-			const nodes = root.descendants()
-			const simulation = d3.forceSimulation(nodes)
-				.force('link', d3.forceLink(links).id(d => d.id).distance(0).strength(1))
-				.force('charge', d3.forceManyBody().strength(-50))
-				.force('x', d3.forceX())
-				.force('y', d3.forceY())
-			const svg = d3.select(document.body).append("svg")
-				.attr("width", width)
-				.attr("height", height)
-				.attr("viewBox", [-(width/2), -(height/2), width, height])
-			const link = svg.append("g")
-				.attr("stroke", "#999")
-				.attr("stroke-opacity", 0.6)
-				.selectAll("line")
-				.data(links)
-				.join("line")
-			const node = svg.append("g")
-				.attr("fill", "#fff")
-				.attr("stroke", "#000")
-				.attr("stroke-width", 1.5)
-				.selectAll("circle")
-				.data(nodes)
-				.join("circle")
-				.attr("fill", d => d.children ? null : "#000")
-				.attr("stroke", d => d.children ? null : "#fff")
-				.attr("r", 3.5)
-			// .call(drag(simulation))
-			node.append("title")
-				.text(d => d.data.name)
-			simulation.on("tick", () => {
-				link
-					.attr("x1", d => d.source.x)
-					.attr("y1", d => d.source.y)
-					.attr("x2", d => d.target.x)
-					.attr("y2", d => d.target.y)
-				node
-					.attr("cx", d => d.x)
-					.attr("cy", d => d.y)
-			})
-		</script>
-		</html>
-	`;
+        this.update();
+        this._panel.webview.postMessage({ type: "force-directed-graph", payload: tree });
     }
 
 }
