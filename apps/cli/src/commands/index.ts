1183952916ffd0eea219f4a48ac15f4453d45893
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-01-05 00:04:57 +0530

    Minor Refactor

--- apps/cli/src/commands/index.ts
+++ apps/cli/src/commands/index.ts
@@ -1,11 +1,16 @@
+// ========
+// COMMANDS
+// ========
+
 // Description: This file is the entry point for all commands. It is used to export all commands to the main file.
+
 import snapshot from './snapshot.js';
 import graph from './graph.js';
 import radial from './radial.js';
 
 // Export commands
 export const commands: Command<any>[] = [
-    snapshot,
     graph,
-    radial
+    radial,
+    snapshot,
 ];
